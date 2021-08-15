17ec952d436fb5f8221f9b79ffcfdca3d3cf7274
moritzbuck <mrtz.buck@gmail.com>
2021-08-15 18:19:03 +0200

    new sim

--- MEvoSim/src/test/test.go
+++ MEvoSim/src/test/test.go
@@ -8,35 +8,57 @@ import ("fmt"
 
 func main() {
   l := log.New(os.Stderr, "", 0)
-  ori_genome := MEvoSim.Fasta2CodonGenome("genomes/ecoli.ffn")
+  ori_genome := MEvoSim.Fasta2CodonGenome("PROKKA_03032020.ffn")
 
 //   for i:= 0; i< 10; i++{
 //   shuffle_genome := genome.ShuffleCodons()
 //   fmt.Println(ori_genome.Similarity(shuffle_genome))
 // }
-  rand_genomes := make([]*MEvoSim.CodonGenome, 20)
-  prop_genomes := make([]*MEvoSim.CodonGenome, 20)
+  nb_genomes := 1
+  genome_pop := make([]*MEvoSim.CodonGenome, nb_genomes)
+  genome_pop[0] = ori_genome.Mutate(0.0, false)
 
-  for j:= 0; j < 20; j++{
-    rand_genomes[j] = ori_genome.Mutate(0.0, false)
-    prop_genomes[j] = ori_genome.Mutate(0.0, false)
-  }
-
-  fmt.Println("it", "rep", "mut_rate", "realANI", "mut_type")
+  fmt.Println("it", "nb_genomes", "mut_rate", "meanANI", "minANI", "anis")
   rate := 0.05
-  for i := 0; i < 300; i++{
-    tt_rand_genomes := make([]*MEvoSim.CodonGenome, 20)
-    tt_prop_genomes := make([]*MEvoSim.CodonGenome, 20)
-    for j := 0; j < 20; j++{
-      tt_rand_genomes[j] = rand_genomes[j].Mutate(rate, false)
-      realANI := ori_genome.Similarity(tt_rand_genomes[j])
-      fmt.Println(i, j, realANI, "random")
-      tt_prop_genomes[j] = prop_genomes[j].Mutate(rate, true)
-      propANI := ori_genome.Similarity(tt_prop_genomes[j])
-      fmt.Println(i, j, propANI, "proportional")
-      l.Println("generation : ", i, " rep: ", j , " random : ", realANI, " proportional : ", propANI)
+
+  for i := 0; i < 30; i++{
+    next_genomes := nb_genomes * 2
+    if nb_genomes == 256{
+      next_genomes = 256
     }
-    rand_genomes = tt_rand_genomes
-    prop_genomes = tt_prop_genomes
+    next_gen := make([]*MEvoSim.CodonGenome, next_genomes)
+    anis := make([]float64, (next_genomes*next_genomes-next_genomes)/2)
+
+    for j := 0; j < nb_genomes; j++{
+      if nb_genomes == 256{
+        next_gen[j] = genome_pop[j].Mutate(rate, false)
+      } else {
+      next_gen[j*2] = genome_pop[j].Mutate(rate, false)
+      next_gen[j*2+1] = genome_pop[j]
+      }
+    }
+    counter := 0
+    min := 1.0
+    mean := 0.0
+    ani_string := ""
+    for k := 0; k < next_genomes ; k++{
+      for l := 0; l < next_genomes; l++{
+        if l > k {
+          ani := next_gen[k].Similarity(next_gen[l])
+          anis[counter] = ani
+          mean += ani
+          ani_string += fmt.Sprintf("%f;",ani)
+          if ani < min {
+            min = ani
+          }
+          counter ++
+        }
+      }
+    }
+    mean = mean/float64(counter)
+    fmt.Println(i, next_genomes, mean, min, ani_string)
+    l.Println("generation : ", i, " nb_genomes: ", next_genomes , " meanANI : ", mean, " minANIs : ", min)
+    genome_pop = next_gen
+    nb_genomes = next_genomes
   }
 }
